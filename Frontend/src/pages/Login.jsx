7a8185636b64a6d67a85a23a53e1532bc6618a72
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-05 21:30:41 +0300

    Add login page

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import frame from "../assets/Frame 1948754931.png";
+
+function Login() {
+  return (
+    <div>
+      <img src={frame} alt="frame" />
+    </div>
+  );
+}
+
+export default Login;
