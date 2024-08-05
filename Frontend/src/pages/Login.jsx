3e916ecfaa6da39ccd32cc93b56675bae2e79678
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-05 23:38:24 +0300

    update login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,16 @@
-import frame from "../assets/Frame 1948754931.png";
+// import group1 from "../assets/Group 1 white.png";
 
 function Login() {
   return (
-    <div>
-      <img src={frame} alt="frame" />
+    <div className="flex h-screen w-full border-4 border-red-500">
+      <div className="bg-[#171B36] h-full w-[50%] flex border-4 border-red-500">
+        {/* <img
+          src={group1}
+          alt="frame"
+            className="h-full w-full object-contain"
+        /> */}
+      </div>
+      <div className="bg-white h-full w-[50%] flex border-4 border-red-500"></div>
     </div>
   );
 }
