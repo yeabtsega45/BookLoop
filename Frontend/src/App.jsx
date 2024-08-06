4304633cba03944b2bfcbbd1d3696bc2c4f17230
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-06 20:17:10 +0300

    create sidebar for the dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import OwnerDashboard from "@/pages/owner/OwnerDashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<OwnerDashboard />} />
       </Routes>
     </>
   );
---
+++ Frontend/src/pages/owner/OwnerDashboard.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/SideBar";
+
+function OwnerDashboard() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default OwnerDashboard;
