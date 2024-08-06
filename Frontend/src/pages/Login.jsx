b45e81c16db258ac542b416eacb01951e19cad72
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-06 12:43:50 +0300

    add color variables

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import group1white from "../assets/Group 1 white.png";
-import group1 from "../assets/Group 1.png";
+import group1white from "@/assets/Group 1 white.png";
+import group1 from "@/assets/Group 1.png";
 
 function Login() {
   return (
     <div className="flex h-screen w-full">
-      <div className="bg-[#171B36] h-full w-[50%] flex justify-center items-center">
+      <div className="bg-secondary h-full w-[50%] flex justify-center items-center">
         <img
           src={group1white}
           alt="group1white"
@@ -12,8 +12,10 @@ function Login() {
         />
       </div>
       <div className="bg-white h-full w-[50%] flex justify-center items-center">
-        <img src={group1} alt="group1" />
-        <h2>Book Rent</h2>
+        <div>
+          <img src={group1} alt="group1" />
+          <h2>Book Rent</h2>
+        </div>
       </div>
     </div>
   );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "var(--color-primary-light)",
+          DEFAULT: "var(--color-primary)",
+          dark: "var(--color-primary-dark)",
+        },
+        secondary: {
+          light: "var(--color-secondary-light)",
+          DEFAULT: "var(--color-secondary)",
+          dark: "var(--color-secondary-dark)",
+        },
+      },
+    },
   },
   plugins: [],
 };
