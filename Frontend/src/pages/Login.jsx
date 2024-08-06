a22dd5423657a1b12d725ade0ca2bc2746ee7c3d
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-06 12:18:57 +0300

    update login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,16 +1,20 @@
-// import group1 from "../assets/Group 1 white.png";
+import group1white from "../assets/Group 1 white.png";
+import group1 from "../assets/Group 1.png";
 
 function Login() {
   return (
-    <div className="flex h-screen w-full border-4 border-red-500">
-      <div className="bg-[#171B36] h-full w-[50%] flex border-4 border-red-500">
-        {/* <img
-          src={group1}
-          alt="frame"
-            className="h-full w-full object-contain"
-        /> */}
+    <div className="flex h-screen w-full">
+      <div className="bg-[#171B36] h-full w-[50%] flex justify-center items-center">
+        <img
+          src={group1white}
+          alt="group1white"
+          // className="h-full w-full object-contain"
+        />
       </div>
-      <div className="bg-white h-full w-[50%] flex border-4 border-red-500"></div>
+      <div className="bg-white h-full w-[50%] flex justify-center items-center">
+        <img src={group1} alt="group1" />
+        <h2>Book Rent</h2>
+      </div>
     </div>
   );
 }
