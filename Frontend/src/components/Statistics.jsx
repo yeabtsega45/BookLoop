890946f091aa49dcfd558f6ee922d6ce7c3f69a0
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-07 21:25:42 +0300

    add mui chart to statistics

--- Frontend/src/components/Statistics.jsx
+++ Frontend/src/components/Statistics.jsx
@@ -1,3 +1,13 @@
+import { PieChart } from "@mui/x-charts/PieChart";
+
+const chartData = [
+  { label: "Fiction", value: 54 },
+  { label: "Self Help", value: 20 },
+  { label: "Business", value: 26 },
+];
+
+const palette = ["blue", "green", "red"];
+
 function Statistics() {
   return (
     <div className="w-[27.7%] h-full m-auto bg-primary-contrast px-4">
@@ -9,7 +19,7 @@ function Statistics() {
           Tue, 14 Nov, 2024, 11.30 AM{" "}
         </p>
       </div>
-      <div className="flex flex-col pb-9 px-6">
+      <div className="flex flex-col pb-9 px-6 rounded shadow-2xl">
         <div className="flex justify-between items-center text-[#656575]">
           <p className="text-lg">Income</p>
           <p className="bg-[#F4F5F7] text-xs p-1 rounded-sm mb-3">This Month</p>
@@ -22,9 +32,47 @@ function Statistics() {
         <p className="text-[#656575] text-sm mb-1">
           Compared to ETB9940 last month
         </p>
-        <div className="flex justify-between items-center text-[#525256] text-sm">
+        <div className="flex justify-between text-[#525256] text-sm">
           <p>Last Month Income</p>
           <p>ETB 25658.00</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded shadow-2xl mt-9 px-4">
+        <div className="flex justify-between text-[#656575] mt-6 px-6 mb-7">
+          <p className="text-lg">Available Books</p>
+          <p className="bg-[#F4F5F7] text-xs p-1 rounded-sm mb-3">Today</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <PieChart
+            series={[
+              {
+                innerRadius: 60,
+                outerRadius: 80,
+                data: chartData,
+              },
+            ]}
+            width={400}
+            height={250}
+            colors={palette}
+            slotProps={{
+              legend: { hidden: true },
+            }}
+            margin={{ right: 5 }}
+          />
+          <div className="w-full flex px-6">
+            <ul className="list-none p-0">
+              {chartData.map((item, index) => (
+                <li key={index} className="flex items-center mb-1">
+                  <span
+                    className="w-4 h-4 mr-2 rounded-full"
+                    style={{ backgroundColor: palette[index] }}
+                  ></span>
+                  <p className="text-[#1A1919]">{item.label}</p>
+                  <p className="text-[#1A1919]">{item.value}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
