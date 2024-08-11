92e423806afb350116c16247ee9dc776ec982982
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-11 11:26:34 +0300

    create user model

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5173"],
     // origin: ["https://bookrent.onrender.com"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
