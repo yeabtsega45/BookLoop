f636206fb9a5f10863d460a4a680e697f430d8be
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-11 11:02:01 +0300

    create index server file

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./config/connectDb");
+const authController = require("./controllers/authController");
+const bookController = require("./controllers/bookController");
+
+const app = express();
+connectDb();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    // origin: ["https://bookrent.onrender.com"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use("/images", express.static("public/images"));
+
+app.use("/auth", authController);
+app.use("/book", bookController);
+
+app.listen(process.env.PORT, () =>
+  console.log("server has been started successfully!")
+);
