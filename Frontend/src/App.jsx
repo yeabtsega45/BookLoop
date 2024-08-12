cf04d884c3a35ecab94b2d7b1f820a8958bff2e9
yeabtsega45 <yabtsega2022@gmail.com>
2024-08-12 21:56:45 +0300

    deploying

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,7 +10,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import NotFound from "./pages/NotFound";
-// import AuthChecker from "./utils/AuthCheker";
+import AuthChecker from "./utils/AuthCheker";
 
 function App() {
   const location = useLocation();
@@ -20,41 +20,43 @@ function App() {
 
   return (
     <>
-      {/* <AuthChecker> */}
-      <div className="relative w-full max-w-[100vw] h-full flex items-center m-auto bg-primary-background  gap-5">
-        {canShowSidebar && (
-          <div>
-            {" "}
-            {showSidebar ? (
-              <SideBar toggleSidebar={() => toggleSidebar(!showSidebar)} />
-            ) : (
-              <div
-                onClick={() => toggleSidebar(!showSidebar)}
-                className="absolute top-8 left-2  self-start bg-blue-950 p-1 rounded-md"
-              >
-                <MenuIcon
-                  sx={{
-                    color: theme.palette.secondary.contrastText,
-                  }}
-                />
-              </div>
-            )}
+      <AuthChecker>
+        <div className="relative w-full max-w-[100vw] h-full flex items-center m-auto bg-primary-background  gap-5">
+          {canShowSidebar && (
+            <div>
+              {" "}
+              {showSidebar ? (
+                <SideBar toggleSidebar={() => toggleSidebar(!showSidebar)} />
+              ) : (
+                <div
+                  onClick={() => toggleSidebar(!showSidebar)}
+                  className="absolute top-8 left-2  self-start bg-blue-950 p-1 rounded-md"
+                >
+                  <MenuIcon
+                    sx={{
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  />
+                </div>
+              )}
+            </div>
+          )}
+          <div
+            className={`flex-1 ${
+              showSidebar ? " h-[95.8%] " : "w-full h-full"
+            }`}
+          >
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<OwnerDashboard />} />
+              <Route path="/bookupload" element={<BookUpload />} />
+              <Route path="/admin/owners" element={<AdminOwners />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </div>
-        )}
-        <div
-          className={`flex-1 ${showSidebar ? " h-[95.8%] " : "w-full h-full"}`}
-        >
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<OwnerDashboard />} />
-            <Route path="/bookupload" element={<BookUpload />} />
-            <Route path="/admin/owners" element={<AdminOwners />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
         </div>
-      </div>
-      {/* </AuthChecker> */}
+      </AuthChecker>
     </>
   );
 }
